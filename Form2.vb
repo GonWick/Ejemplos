5b7939a95dc9eac516e6ba700db2a854ac5acc61
GonWick <gonxza@gmail.com>
2020-12-18 12:58:53 +0100

    Retoques 1

--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,14 @@
 ﻿Public Class FormUML
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
 
+    Private Sub FormUML_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub FormUML_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+        Me.Close()
+
+    End Sub
 End Class
